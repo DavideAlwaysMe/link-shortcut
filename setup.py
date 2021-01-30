3f3e57ed5b9f129a55ccce23f7af6e90bd4bb7dc
DavideAlwaysMe <51715104+DavideAlwaysMe@users.noreply.github.com>
2021-01-30 10:54:10 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,19 @@
+import os
+from setuptools import setup
+
+
+setup(
+    name = "link",
+    version = "0.1",
+    author = "Davide Rizzuto",
+    author_email = "yodadr01@gmail.com",
+    license = "MIT",
+    url = "https://github.com/DavideAlwaysMe/link-shortcut",
+    packages=['link'],
+    scripts = ['link/link.py'],
+    data_files = [
+        ('/usr/share/applications', ['link.desktop'])
+    ],
+    package_data = {'link': ['link.glade','icona.png']},
+    install_requires = [ 'requests','favicon'],
+)
